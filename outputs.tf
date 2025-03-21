36a391bdb9d4b8097af93b327385a08e863798cd
muralimupparaju <muralimupparaju@gmail.com>
2025-03-21 15:09:49 -0400

    update dated it

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "ec2_public_ip" {
-  value = aws_instance.wordpress_vm.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.wordpress_vm.public_ip
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.wordpress_db.endpoint
+  description = "Endpoint of the RDS instance"
+  value       = aws_db_instance.wordpress_db.endpoint
 }
